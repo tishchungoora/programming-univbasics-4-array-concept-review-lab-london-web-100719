0412d684e45d812664e22ba0f264465987718571
Tish Chungoora <tishal82@gmail.com>
2019-09-16 17:41:31 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,8 +3,8 @@ end
 
 def find_max_number(array)
   max_num = array.first
-  array.length.times { |x|
-  if max_num < array[x] then max_num = array[x] end
+  array.length.times { |index|
+  if max_num < array[index] then max_num = array[index] end
   }
   max_num
 end
