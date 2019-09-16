9d0ac80fcbf99601a6a27fbf361c3e7d66b1a6b2
Tish Chungoora <tishal82@gmail.com>
2019-09-16 17:17:07 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,8 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_number(array)
-  array.length.times { |x| if max_num < number[x] then max_num = number[x]
+  array.length.times { |x|
+  if max_num < number[x] then max_num = number[x] end }
 end
 
 =begin
