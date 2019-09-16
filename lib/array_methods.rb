775e7c522253f2b5fe3ea3135445ee3fd7dbd91c
Tish Chungoora <tishal82@gmail.com>
2019-09-16 17:15:30 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,15 @@
 def find_element_index(array, value_to_find)
 end
 
+def find_max_number(array)
+  array.length.times { 
+end
+
+=begin
 def find_max_value(array)
   array.max
 end
+=end
 
 def find_min_value(array)
   array.min
